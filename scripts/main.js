017dadac4ea4dfd5cd5b5e2f81d3c21127df6e7e
ymsang <ymsang@mo9.com>
2018-12-25 17:15:14 +0800

    support markdown

--- scripts/main.js
+++ scripts/main.js
@@ -33,10 +33,12 @@ function toMarkdown(str) {
   // log('----------------------------------------')
   return clone
 }
-function convert(text) {
+function convert(text, inputType = 'plain') {
   if (text) {
-    let md = toMarkdown(text)
+    let md = inputType === 'markdown' ? text : toMarkdown(text)
+
     let html = converter.makeHtml(md)
+
     const fullHtml = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Document</title><style>li,p,br {margin:0.4rem 0} html {font-size: 14px;line-height: 180%;text-align: justify;}html table {border-collapse: collapse;color: #3e3e3e;}html table,html th,html td {border: 1px solid #3e3e3e;font-size: 1rem;padding: 1rem;}html body .wrapper {margin: 0 auto;padding: 2rem;/* font-size: 0.8rem; */color: #3e3e3e;max-width: 800px;}html body .wrapper h1 {font-size: 1.2rem;color: #3367d6;text-align: center;}html body .wrapper h2 {font-size: 1.1rem;color: #3367d6;border-bottom: 1px solid #3367d6;padding-bottom: 0.5rem;}html body .wrapper .footer {text-align: right;}</style></head><body><div class="wrapper">${html}</div></body></html>`
     return fullHtml
   }
@@ -54,11 +56,12 @@ function main() {
   let btn = document.querySelector('.btn')
   btn.addEventListener('click', function (e) {
     let text = input.value
-    let html = convert(text)
-    // log(html)
+    let inputType = document.querySelector('input[name="inputType"]:checked').value
+    let html = convert(text, inputType)
     if (html) {
       output.value = html
     }
+
   }, false)
 
 
